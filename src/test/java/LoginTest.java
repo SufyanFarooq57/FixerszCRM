89c6963ee79624357db26dbe9a33630f2d49d660
SufyanFarooq57 <farooqsufyan701@gmail.com>
2026-07-27 17:59:39 +0500

    Updated Java version to JDK 21

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,8 @@
-import org.testng.Assert;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
-import pages.DashboardPage;
 import pages.LoginPage;
 
 public class LoginTest extends  BaseTest{
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -2,8 +2,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
-import pages.DashboardPage;
-import pages.LoginPage;
 import pages.Tasks;
 
 public class TaskTest extends BaseTest{
