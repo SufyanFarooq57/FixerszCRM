36f07a8bff3f9890ab077e751d422e4c1bb60451
SufyanFarooq57 <farooqsufyan701@gmail.com>
2026-07-27 18:16:08 +0500

    Added DriverFactory and fixed framework structure

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,15 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+	public static WebDriver initializeDriver() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+
+}
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,7 +3,9 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 
+import driver.DriverFactory;
 import pages.LoginPage;
+
 public class BaseTest {
 	public WebDriver driver;
 
